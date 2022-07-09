007e2529133b4c2d7be70b17c645c20e0ed09f3e
udothemath <fuchang.sun@gmail.com>
2022-07-09 23:17:11 +0800

    docs: edit readme

--- README.md
+++ README.md
@@ -11,8 +11,10 @@
 
 - [Template Designer Documentation](https://jinja.palletsprojects.com/en/3.0.x/templates/)
 
-- https://geek-docs.com/python/python-tutorial/python-jinja.html
+- [postgresql@runoob](https://www.runoob.com/postgresql/postgresql-with.html)
 
-- https://tendcode.com/article/yaml_and_jinja2/
+- [python-jinja](https://geek-docs.com/python/python-tutorial/python-jinja.html)
+
+- (yaml_and_jinja2)[https://tendcode.com/article/yaml_and_jinja2/]
 - [A Simple Approach To Templated SQL Queries In Python](https://towardsdatascience.com/a-simple-approach-to-templated-sql-queries-in-python-adc4f0dc511)
 - [Advanced SQL Templates In Python with JinjaSql](https://towardsdatascience.com/advanced-sql-templates-in-python-with-jinjasql-b996eadd761d)
--- templates/demo_sql_pool.py
+++ templates/demo_sql_pool.py
@@ -1,20 +1,20 @@
 sql_v7 = ''' 
-        -- Comments: {{table3.comment}} 
-        SELECT {%
-        for _, value in table3.variable.items
-        () %}
-            {{value}},{% endfor %}
-            1 as dummy
-        FROM "{{schema}}.{{table3.tablename}}"
-    '''
+-- Comments: {{table3.comment}} 
+SELECT {%
+for _, value in table3.variable.items
+() %}
+    {{value}},{% endfor %}
+    1 as dummy
+FROM "{{schema}}.{{table3.tablename}}"
+'''
 sql_v8 = ''' 
-        -- Comments: Left join
-        SELECT {%for _, value in table3.variable.items() %}
-            {{table3.tablename_as}}.{{value}},{% endfor %}
-            {{table4.tablename_as}}.{{table3.variable.varname1}}
-            {{table4.condition.cond1}}({{table4.tablename_as}}.{{table4.variable.varname2}}) AS total_spent
-        FROM {{schema}}.{{table3.tablename}} {{table3.tablename_as}}
-        LEFT JOIN {{schema}}.{{table4.tablename}} {{table4.tablename_as}}
-        ON {{table3.tablename_as}}.{{table3.variable.varname1}} = {{table4.tablename_as}}.{{table4.variable.varname1}}
-        GROUP BY {{table3.tablename_as}}.{{table3.variable.varname1}}
-    '''
+-- Comments: Left join
+SELECT {%for _, value in table3.variable.items() %}
+    {{table3.tablename_as}}.{{value}},{% endfor %}
+    {{table4.tablename_as}}.{{table3.variable.varname1}}
+    {{table4.condition.cond1}}({{table4.tablename_as}}.{{table4.variable.varname2}}) AS total_spent
+FROM {{schema}}.{{table3.tablename}} {{table3.tablename_as}}
+LEFT JOIN {{schema}}.{{table4.tablename}} {{table4.tablename_as}}
+ON {{table3.tablename_as}}.{{table3.variable.varname1}} = {{table4.tablename_as}}.{{table4.variable.varname1}}
+GROUP BY {{table3.tablename_as}}.{{table3.variable.varname1}}
+'''

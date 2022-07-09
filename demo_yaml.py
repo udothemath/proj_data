f5a836da80b43d00725d5dddc0ff1725d6fc76f1
udothemath <fuchang.sun@gmail.com>
2022-07-09 12:21:58 +0800

    feat: add html demo

--- demo_yaml.py
+++ demo_yaml.py
@@ -1,9 +1,26 @@
+# from typing import Any, Type
+from jinja2 import Environment, FileSystemLoader, Template
 from yaml import safe_load
 
 
-def quick_demo_yaml1():
-    filename = 'demo_yaml_file.yml'
+def quick_demo_yaml1(filename: str) -> str:
     with open(filename, 'r') as f:
         vars_string = f.read()
     base_vars = safe_load(vars_string)
-    print(base_vars)
+    return base_vars
+
+
+def quick_demo_yaml2(filename: str) -> str:
+    with open(filename, 'r') as f:
+        vars_string = f.read()
+    base_vars = safe_load(vars_string)
+    parsed_yaml = Template(vars_string).render(base_vars)
+    return parsed_yaml
+
+
+def quick_demo_yaml3(vars: str, filename: str) -> str:
+    load = FileSystemLoader('templates')
+    env = Environment(loader=load)
+    template = env.get_template(filename)
+    result = template.render(vars)
+    return result
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # %%
 from demo_jinja import quick_demo_jinja1, quick_demo_jinja2
-from demo_yaml import quick_demo_yaml1
+from demo_yaml import quick_demo_yaml1, quick_demo_yaml2, quick_demo_yaml3
 
 if __name__ == "__main__":
     print("Start with example")
     quick_demo_jinja1()
     quick_demo_jinja2()
-    quick_demo_yaml1()
+
+    file_yaml = 'demo_yaml_file.yml'
+    yaml1 = quick_demo_yaml1(filename=file_yaml)
+    yaml2 = quick_demo_yaml2(filename=file_yaml)
+    print(yaml1)
+    # print(yaml2)
+
+    file_html = 'demo_html_file.html'
+    yaml3 = quick_demo_yaml3(vars=yaml1, filename=file_html)
+    print(yaml3)
+
     print("Done")
-# %%

fc11ccb08ce728b0bc03ff59330d61482cfd5e7f
udothemath <fuchang.sun@gmail.com>
2022-07-09 12:59:09 +0800

    feat: add sql_v1 demo

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ from demo_jinja import quick_demo_jinja1, quick_demo_jinja2
 from demo_yaml import quick_demo_yaml1, quick_demo_yaml2, quick_demo_yaml3
 
 if __name__ == "__main__":
-    print("Start with example")
-    quick_demo_jinja1()
-    quick_demo_jinja2()
+    print(" --- Start --- ")
+    j1 = quick_demo_jinja1()
+    j2 = quick_demo_jinja2()
+    # print(j1)
+    # print(j2)
 
     file_yaml = 'demo_yaml_file.yml'
     yaml1 = quick_demo_yaml1(filename=file_yaml)
     yaml2 = quick_demo_yaml2(filename=file_yaml)
-    print(yaml1)
+    # print(yaml1)
     # print(yaml2)
 
     file_html = 'demo_html.html'
     yaml3 = quick_demo_yaml3(vars=yaml1, filename=file_html)
-    print(yaml3)
+    # print(yaml3)
 
     file_yaml_for_sql = 'demo_yaml_sql.yml'
-    file_sql = 'demo_sql_file.sql'
-    yaml3 = quick_demo_yaml3(vars=yaml1, filename=file_html)
-    print(yaml3)
+    var = quick_demo_yaml1(filename=file_yaml_for_sql)
+    file_sql = 'demo_sql_v1.sql'
+    result4 = quick_demo_yaml3(vars=var, filename=file_sql)
+    print(result4)
 
-    print("Done")
+    print(" --- End --- ")
---
+++ templates/demo_sql_v1.sql
@@ -0,0 +1,6 @@
+select
+    {{table1
+.query.variable.var1}},
+    {{table1.query.variable.var2}}
+from "{{schema}}.{{table1.tablename}}"
+where 

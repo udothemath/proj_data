60fa815fa2740c657c686dbfaad1d931dd9b7e81
udothemath <fuchang.sun@gmail.com>
2022-08-02 22:16:57 +0800

    feat: add readme.md for demo_plugin

--- main.py
+++ main.py
@@ -15,32 +15,32 @@ if __name__ == "__main__":
     print(" ==== Start ==== ")
     j1 = quick_demo_jinja1()
     j2 = quick_demo_jinja2()
-    # print(j1)
-    # print(j2)
+    print(j1)
+    print(j2)
 
     file_yaml = 'demo_yaml_basic.yml'
     yaml1 = quick_demo_yaml1(filename=file_yaml)
     yaml2 = quick_demo_yaml2(filename=file_yaml)
-    # print(yaml1)
-    # print(yaml2)
+    print(yaml1)
+    print(yaml2)
 
     file_html = 'demo_html.html'
     yaml3 = quick_demo_yaml3(vars=yaml1, filename=file_html)
-    # print(yaml3)
+    print(yaml3)
 
     yaml_vars = yaml_setting()
 
     file_sql = 'demo_sql_v1.sql'
     result4 = quick_demo_yaml3(vars=yaml_vars, filename=file_sql)
-    # print(result4)
+    print(result4)
 
     file_sql = 'demo_sql_v2.sql'
     result5 = quick_demo_yaml3(vars=yaml_vars, filename=file_sql)
-    # print(result5)
+    print(result5)
 
     file_sql = 'demo_sql_v3.sql'
     result6 = quick_demo_yaml3(vars=yaml_vars, filename=file_sql)
-    # print(result6)
+    print(result6)
 
     from templates.demo_sql_pool import sql_v7, sql_v8
     result = Template(sql_v7).render(yaml_vars)

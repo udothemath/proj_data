61352d349d39ba6188521e4fd92bbe9d8c9a3c7d
udothemath <fuchang.sun@gmail.com>
2022-08-21 14:56:23 +0800

    feat: refactor use_cache code

--- demo_speed_up/use_cache.py
+++ demo_speed_up/use_cache.py
@@ -1,8 +1,22 @@
 import os
+import sys
 import time
 from functools import lru_cache, wraps
 
-from memory_profiler import memory_usage, profile
+from memory_profiler import LogFile, memory_usage, profile
+
+
+# sys.stdout = LogFile('memory_profile_log', reportIncrementFlag=False)
+def get_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - start
+        print(f"func: {func.__qualname__}. Elapsed: {elapsed:.2f} second.")
+        return result
+    return wrapper
+
 
 log_file = open('profile.log', 'w+')
 
@@ -28,37 +42,14 @@ def fib(n):
     return fib(n-1) + fib(n-2)
 
 
+@get_time
+@profile(stream=log_file)
 def main(func, n_size=30):
     for i in range(n_size+1):
         print(i, func(i))
     print("done")
 
 
-def get_time(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        elapsed = time.perf_counter() - start
-        print(f"func: {func.__qualname__}. Elapsed: {elapsed:.2f} second.")
-        return result
-    return wrapper
-
-
-@get_time
-def say_hi():
-    time.sleep(0.5)
-    print("hello")
-
-
-def main_v2():
-    say_hi()
-
-
 if __name__ == '__main__':
-    main_v2()
-    # start = time.perf_counter()
-    # # main(func=fib_cache, n_size=100)
-    # my_func()
-    # elapsed = time.perf_counter() - start
-    # print(f"Elapsed: {elapsed:.2f} second.")
+    main(func=fib, n_size=20)
+    main(func=fib_cache, n_size=200)

b48ded50908336dee559a2f86a276a4bbb54cb48
udothemath <fuchang.sun@gmail.com>
2022-08-21 14:39:23 +0800

    feat: add functools.wraps

--- demo_speed_up/use_cache.py
+++ demo_speed_up/use_cache.py
@@ -1,6 +1,6 @@
 import os
 import time
-from functools import lru_cache
+from functools import lru_cache, wraps
 
 from memory_profiler import memory_usage, profile
 
@@ -34,11 +34,31 @@ def main(func, n_size=30):
     print("done")
 
 
+def get_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - start
+        print(f"func: {func.__qualname__}. Elapsed: {elapsed:.2f} second.")
+        return result
+    return wrapper
+
+
+@get_time
+def say_hi():
+    time.sleep(0.5)
+    print("hello")
+
+
+def main_v2():
+    say_hi()
+
+
 if __name__ == '__main__':
-    start = time.perf_counter()
-    # main(func=fib_cache, n_size=100)
-
-    my_func()
-
-    elapsed = time.perf_counter() - start
-    print(f"Elapsed: {elapsed:.2f} second.")
+    main_v2()
+    # start = time.perf_counter()
+    # # main(func=fib_cache, n_size=100)
+    # my_func()
+    # elapsed = time.perf_counter() - start
+    # print(f"Elapsed: {elapsed:.2f} second.")
